78ead970af45826b96916f43a7436812722ca702
Maxime Jamotte <jamottemax@gmail.com>
2018-04-21 16:23:15 +0200

    Fonction externes + finalisation 1

---
+++ Conjugate_Gradient.py
@@ -0,0 +1,54 @@
+import numpy
+from matplotlib import pyplot, cm
+from mpl_toolkits.mplot3d import Axes3D
+
+def plot_3D(x, y, p, elev=30, azi=45):
+    '''Creates 3D projection plot with appropriate limits and viewing angle
+    
+    Parameters:
+    ----------
+    x: array of float
+        nodal coordinates in x
+    y: array of float
+        nodal coordinates in y
+    p: 2D array of float
+        calculated potential field
+    
+    '''
+    fig = pyplot.figure(figsize=(11,7), dpi=100)
+    ax = fig.gca(projection='3d')
+    X,Y = numpy.meshgrid(x,y)
+    surf = ax.plot_surface(X,Y,p[:], rstride=1, cstride=1, cmap=cm.viridis,
+            linewidth=0, antialiased=False)
+
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    ax.set_zlabel('$z$')
+    ax.view_init(elev,azi)
+
+def p_analytical(x, y):
+    '''Returns the analytical solution for the given Laplace Problem on a grid
+    with coordinates x and y
+    
+    Parameters:
+    ----------
+    xL array of float
+        Nodal coordinates in x
+    y: array of float
+        Nodal coordinates in y
+        
+    Returns:
+    -------
+    pxy: 2D array of float
+        Potential distribution analytical solution
+    
+    '''
+    X, Y = numpy.meshgrid(x,y)
+    pxy = numpy.sinh(1.5*numpy.pi*Y / x[-1]) /\
+    (numpy.sinh(1.5*numpy.pi*y[-1]/x[-1]))*numpy.sin(1.5*numpy.pi*X/x[-1])
+    
+    return pxy
+
+def L1norm(new, old):
+    norm = numpy.sum(numpy.abs(new-old))
+    return norm
